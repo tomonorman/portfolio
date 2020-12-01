800c80e83f8d344d6de8b3eafccfb7e01c92defb
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-01 21:30:13 +0900

    formatted code to be prettier

--- javascript/transition.js
+++ javascript/transition.js
@@ -1,30 +1,28 @@
 Barba.Pjax.start();
 
 var FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-
-  // As soon the loading is finished and the old page is faded out, let's fade the new page
-  Promise
-  .all([this.newContainerLoading, this.fadeOut()])
-  .then(this.fadeIn.bind(this));
+  start: function () {
+    // As soon the loading is finished and the old page is faded out, let's fade the new page
+    Promise.all([this.newContainerLoading, this.fadeOut()]).then(
+      this.fadeIn.bind(this)
+    );
   },
-  fadeOut: function() {
-    this.oldContainer
+  fadeOut: function () {
+    this.oldContainer;
   },
 
-  fadeIn: function() {
+  fadeIn: function () {
     this.newContainer.classList.add("slide-in");
     const that = this;
-    this.newContainer.addEventListener("animationend", function(){
+    this.newContainer.addEventListener("animationend", function () {
       that.newContainer.classList.remove("slide-in");
       that.done();
     });
-  }
+  },
 });
 /**
-* Next step, you have to tell Barba to use the new Transition
-*/
-Barba.Pjax.getTransition = function() {
-
+ * Next step, you have to tell Barba to use the new Transition
+ */
+Barba.Pjax.getTransition = function () {
   return FadeTransition;
 };
