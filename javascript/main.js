fd7b5a300f199b1f4bca2d3e3133ff29fe450233
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-01 17:00:19 +0900

    intro screen done

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,8 @@
-console.log("Hi Tomo!");
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+
+tl.to(".text1", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".text2", { y: "0%", duration: 1, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1.5, delay: 1 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
+tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
