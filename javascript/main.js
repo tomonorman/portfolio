e873b9a590434681a6a9ce0765e282f426670e04
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-07 08:46:00 +0900

    social media icons fix

--- javascript/main.js
+++ javascript/main.js
@@ -7,3 +7,4 @@ tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
 tl.fromTo(".avatar", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".social-media", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
