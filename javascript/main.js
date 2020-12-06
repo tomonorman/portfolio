e051adf33941b1915d4a7763ade971fed52fd2a9
Tomo Norman <tomo.l.norman@gmail.com>
2020-12-06 11:15:25 +0900

    main page avatar done

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,4 @@ tl.to(".slider", { y: "-100%", duration: 1.5, delay: 1 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
+tl.fromTo(".avatar", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
