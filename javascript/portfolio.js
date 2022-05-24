25ab5bb4ff1f0e817e2243a41b564ec81864bc68
Tomo Norman <tomo.l.norman@gmail.com>
2022-05-24 21:08:54 +0900

    update portfolio

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -11,25 +11,30 @@ if (previews) {
       // Dynamic change of text and image
       const originalSrc = preview.getAttribute("data-original");
       original.src = `../img/${originalSrc}`;
-      if (originalSrc === "syncalyst.png") {
+      if (originalSrc === "dreamfactory-guide.png") {
         imgText.insertAdjacentHTML(
           "afterbegin",
-          "<h3>Syncalyst</h3><p>A meeting assistant with voice recognition and connectivity to project management tools</p><p><i class='pr-1 dev-size devicon-rails-plain colored'></i><i class='dev-size pr-1 devicon-postgresql-plain colored'></i><i class='pr-1 dev-size devicon-heroku-plain colored'></i><i class='pr-1 dev-size devicon-javascript-plain colored'></i><i class='dev-size devicon-trello-plain colored'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://www.syncalyst.com' target='_blank'>See the Site</a><a class='pl-4' href='https://github.com/tomonorman/syncalyst' target='_blank'>See the Code</a></div>"
+          "<h3>DreamFactory Guide</h3><p>Documentation for the DreamFactory Platform</p><p><i class='pr-1 dev-size devicon-hugo-plain colored'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://guide.dreamfactory.com' target='_blank'>See the Site</a><a class='pl-4' href='https://github.com/dreamfactorysoftware/dreamfactory-book-v2' target='_blank'>See the Code</a></div>"
         );
-      } else if (originalSrc === "syncabuddy.png") {
+      } else if (originalSrc === "dfsetup.png") {
         imgText.insertAdjacentHTML(
           "afterbegin",
-          "<h3>Syncabuddy</h3><p>A marketplace allowing users to join sports events taking place near them</p><p><i class='pr-1 dev-size devicon-rails-plain colored'></i><i class='dev-size pr-1 devicon-postgresql-plain colored'></i><i class='pr-1 dev-size devicon-heroku-plain colored'></i><i class='dev-size devicon-javascript-plain colored'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://syncabuddy.herokuapp.com/' target='_blank'>See the Site</a><a class='pl-4' href='https://github.com/tomonorman/Syncabuddy' target='_blank'>See the Code</a></div>"
+          "<h3>DreamFactory Installer</h3><p>Install DreamFactory easily on Ubuntu, Fedora, RHEL, and Debian</p><p><i class='pr-1 dev-size devicon-bash-plain'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://github.com/dreamfactorysoftware/dreamfactory/blob/master/installers/dfsetup.run' target='_blank'>Download!</a><a class='pl-4' href='https://github.com/dreamfactorysoftware/dreamfactory/tree/master/installers/source' target='_blank'>See the Code</a></div>"
         );
-      } else if (originalSrc === "tomococktail.png") {
+      } else if (originalSrc === "dreamfactory.png") {
         imgText.insertAdjacentHTML(
           "afterbegin",
-          "<h3>Tomo's Cocktails</h3><p>A cocktail recipe organizer</p><p><i class='pr-1 dev-size devicon-rails-plain colored'></i><i class='dev-size pr-1 devicon-postgresql-plain colored'></i><i class='pr-1 dev-size devicon-heroku-plain colored'></i><i class='dev-size devicon-javascript-plain colored'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://tomos-cocktails.herokuapp.com/' target='_blank'>See the Site</a><a class='pl-4' href='https://github.com/tomonorman/rails-mister-cocktail' target='_blank'>See the Code</a></div>"
+          "<h3>DreamFactory</h3><p>Instantly generate REST APIs</p><p><i class='pr-1 dev-size devicon-php-plain colored'></i><i class='pr-1 dev-size devicon-laravel-plain-wordmark colored'></i><i class='pr-1 dev-size devicon-angularjs-plain colored'></i><i class='pr-1 dev-size devicon-nginx-original colored'></i><i class='pr-1 dev-size devicon-mysql-plain colored'></i><i class='pr-1 dev-size devicon-composer-line'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://www.dreamfactory.com/' target='_blank'>Homepage</a><a class='pl-4' href='https://github.com/dreamfactorysoftware/dreamfactory' target='_blank'>See the Code</a></div>"
         );
       } else if (originalSrc === "portfolio.png") {
         imgText.insertAdjacentHTML(
           "afterbegin",
           "<h3>This Website!</h3><p><i class='pr-1 dev-size devicon-html5-plain colored'></i><i class='pr-1 dev-size devicon-css3-plain colored'></i><i class='pr-1 dev-size devicon-javascript-plain colored'></i><i class=' dev-size devicon-bootstrap-plain colored'></i></p><div class='project-link pb-2'><a href='https://github.com/tomonorman/portfolio' target='_blank'>See the Code</a></div>"
+        );
+      } else if (originalSrc === "historyboys.png") {
+        imgText.insertAdjacentHTML(
+          "afterbegin",
+          "<h3>History Boys Japan</h3><p>Homepage for the Japanese stage production of The History Boys</p><p><i class='pr-1 dev-size devicon-hugo-plain colored'></i></p><div class='project-link pb-2'><a class='pr-4' href='https://www.historyboysjapan.com' target='_blank'>See the Site</a><a class='pl-4' href='https://github.com/tomonorman/historyboys' target='_blank'>See the Code</a></div>"
         );
       }
     });
